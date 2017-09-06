cd99c970f8ce2a8a80944535e1b71ab64e856623
Gabriel <gabrieldias@risestudio.com.br>
2017-09-06 16:01:21 -0300

    Upando restante dos arquivos

---
+++ src/providers/moovie/moovie.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the MoovieProvider provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular DI.
+*/
+@Injectable()
+export class MoovieProvider {
+  private baseApiPath = "https://api.themoviedb.org/3";
+
+  constructor(public http: Http) {
+    console.log('Hello MoovieProvider Provider');
+  }
+
+  getLastestMovies(){
+    return this.http.get(this.baseApiPath + "/movie/popular?api_key=601a83b5f0d4cf44c91402f1e85238c3");
+  }
+
+}
